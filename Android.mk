3a9bf7cf446de5b6d48962c9de95903b266dc135
Stefan Bender <stfn.bender@gmail.com>
2014-05-25 17:59:14 +0200

    Android.mk: build libomxcore and libopencorehw
    Enables building the libomxcore/ and libopencorehw/ subdirs.

--- Android.mk
+++ Android.mk
@@ -8,6 +8,8 @@ ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),buzz)
         $(LOCAL_PATH)/liblights/Android.mk \
         $(LOCAL_PATH)/librpc/Android.mk \
         $(LOCAL_PATH)/libsensors/Android.mk \
+        $(LOCAL_PATH)/libomxcore/Android.mk \
+        $(LOCAL_PATH)/libopencorehw/Android.mk \
         $(LOCAL_PATH)/libstagefrighthw/Android.mk \
         $(LOCAL_PATH)/libcamera/Android.mk \
         $(LOCAL_PATH)/recovery/offmode/Android.mk
